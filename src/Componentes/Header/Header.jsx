35ac46cba356e8e75410aac82e2d8057de4da3f1
AdriUlima <20200711@aloe.ulima.edu.pe>
2025-06-14 02:38:44 -0500

    avance

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "./Header.css";
 import logo from "../../Images/logo.png"; // Cambia la ruta si usas otra ubicación
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const CerrarSesion = () => {
+    navigate('/')
+  }
+
   return (
     <header className="header">
       <div className="header-left">
@@ -16,7 +23,7 @@ const Header = () => {
         <a href="/filtrogeneral">BUSCAR</a>
         <a href="/perfil">PERFIL</a>
       </nav>
-      <button className="logout-btn">CERRAR SESIÓN</button>
+      <button className="logout-btn" onClick={CerrarSesion}>CERRAR SESIÓN</button>
     </header>
   );
 };
