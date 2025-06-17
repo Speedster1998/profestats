ca0af66240391ddc29e8f80e91ab28a5ed2ae6d2
Sideron <guardian2012cave@gmail.com>
2025-06-17 15:26:58 -0500

    Login Signin catch data

--- src/Pages/SignIn/Signin.jsx
+++ src/Pages/SignIn/Signin.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import styles from "../Login/login.module.css";
 import SigLogLayout from "../LogIn/SigLogLayout.jsx";
 
 const Signin = () => {
-    
-    const handleSignin = () => {
-        console.log("d")
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeat, setRepeat] = useState("");
+
+    const handleSignin = (e) => {
+        e.preventDefault();
+        console.log("usuario:", username);
+        console.log("contraseña:", password);
+        console.log("contraseña repetida:", repeat);
     }
     
     return (
-    <SigLogLayout title="Crea una cuenta" subtitle="Ingresa tu correo y contraseña" buttonText="Registrarse" footer="¿Ya tienes una cuenta?" href="/login" footer2="Inicia sesión" onSubmit={handleSignin} />
+    <SigLogLayout title="Crea una cuenta" subtitle="Ingresa tu correo y contraseña" buttonText="Registrarse" footer="¿Ya tienes una cuenta?" href="/login" footer2="Inicia sesión" onSubmit={handleSignin} emailText={username}
+            setEmail={setUsername}
+            passwordText={password}
+            setPassword={setPassword} repeatPassword={repeat} setRepeat={setRepeat}/>
     )
 }
 
