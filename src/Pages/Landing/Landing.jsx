79fdcb3d69e8b8a1d9b550eaaf447df0b99ee4fd
Sideron <guardian2012cave@gmail.com>
2025-06-17 22:59:49 -0500

    landing ajustes

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -13,26 +13,32 @@ const Landing = () => {
     return (
     <>
     <HeaderLanding onRegisterClick={Registarse}/>
-    <section className={styles.section}>
-        <div className={styles.textSection}>
-            <h1>
-                <span className={styles.highlight}>CALIFICA</span> A TUS PROFESORES<br/>
-                TU <span className={styles.highlight}>OPINIÓN</span> CUENTA
-            </h1>
-            <p>
-                ¡Tu opinión cuenta y puede marcar la diferencia!<br/>
-                ¿Tuviste un profesor increíble que hizo más fácil aprender? ¿O uno que podría mejorar su forma de enseñar?<br/>
-                Calificar y comentar sobre tus profesores no solo te permite expresar tu experiencia, sino que también ayuda a cientos de estudiantes como tú a tomar mejores decisiones académicas.
-            </p>
-            <button className={styles.registerButton} onClick={Registarse}>REGISTRARSE<span className={styles.arrow}>→</span></button>
+
+        <div className='container'>
+            <div className='row align-items-center min-vh-100'>
+                <div className='col-6 text-light'>
+                    <h1 style={{fontSize:45}}>
+                        <span className={styles.highlight}>CALIFICA</span> A TUS PROFESORES<br/>
+                        TU <span className={styles.highlight}>OPINIÓN</span> CUENTA
+                    </h1>
+                    <p>
+                        ¡Tu opinión cuenta y puede marcar la diferencia!<br/>
+                        ¿Tuviste un profesor increíble que hizo más fácil aprender? ¿O uno que podría mejorar su forma de enseñar?<br/>
+                        Calificar y comentar sobre tus profesores no solo te permite expresar tu experiencia, sino que también ayuda a cientos de estudiantes como tú a tomar mejores decisiones académicas.
+                    </p>
+                    <button className={styles.logoutBtn} onClick={Registarse}>REGISTRARSE<span className={styles.arrow}>→</span></button>
+                </div>
+                <div className='col-1'></div>
+                <div className='col-4'>
+                <div className={styles.imageSection}>
+                    <img src={profeLanding} alt="Profesor"/>
+                </div>
+                </div>
+                {<div className={styles.helpIcon}>
+                    <button className={styles.helpButton}>?</button>
+                </div>}
+            </div>
         </div>
-        <div className={styles.imageSection}>
-            <img src={profeLanding} alt="Profesor" />
-        </div>
-        <div className={styles.helpIcon}>
-            <button className={styles.helpButton}>?</button>
-        </div>
-    </section>
     </>
     )
 }
